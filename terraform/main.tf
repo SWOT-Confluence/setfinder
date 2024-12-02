7cf4efa3db8332dfc3df213aaa56f992657c2e4a
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2024-12-02 22:30:45 +0000

    Ensure environment tag is lowercase

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,7 +42,7 @@ locals {
   account_id = data.aws_caller_identity.current.account_id
   default_tags = length(var.default_tags) == 0 ? {
     application : var.app_name,
-    environment : var.environment,
+    environment : lower(var.environment),
     version : var.app_version
   } : var.default_tags
 }
