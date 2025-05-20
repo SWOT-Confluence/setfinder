c2db1a97a6b40d342d09b62503876724beef89b0
Joshua Garde <jgarde@jpl.nasa.gov>
2025-05-19 20:34:18 -0700

    Initial parameterization refactor

--- terraform/modules/setfinder/confluence-setfinder.tf
+++ terraform/modules/setfinder/confluence-setfinder.tf
@@ -1,41 +1,47 @@
 # Job Definition
 resource "aws_batch_job_definition" "generate_batch_jd_setfinder" {
-  name                  = "${var.prefix}-setfinder"
-  type                  = "container"
-  container_properties  = <<CONTAINER_PROPERTIES
-  {
-    "image": "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.prefix}-setfinder",
-    "executionRoleArn": "${data.aws_iam_role.exe_role.arn}",
-    "jobRoleArn": "${data.aws_iam_role.job_role.arn}",
-    "fargatePlatformConfiguration": { "platformVersion": "LATEST" },
-    "logConfiguration": {
-      "logDriver" : "awslogs",
-      "options": {
-        "awslogs-group" : "${data.aws_cloudwatch_log_group.cw_log_group.name}"
-      }
-    },
-    "resourceRequirements": [
-      {"type": "MEMORY", "value": "2048"},
-      {"type": "VCPU", "value": "1"}
-    ],
-    "mountPoints": [
-      {
-        "sourceVolume": "input",
-        "containerPath": "/data"
-      }
-    ],
-    "volumes": [
-      {
-        "name": "input",
-        "efsVolumeConfiguration": {
-          "fileSystemId": "${data.aws_efs_file_system.aws_efs_input.file_system_id}",
-          "rootDirectory": "/"
-        }
-      }
-    ]
-  }
-  CONTAINER_PROPERTIES
+  name = "${var.prefix}-setfinder"
+  type = "container"
   platform_capabilities = ["FARGATE"]
-  propagate_tags        = true
+  propagate_tags = true
   tags = { "job_definition": "${var.prefix}-setfinder" }
+
+  container_properties = jsonencode({
+    image = "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.prefix}-setfinder:${var.image_tag}"
+    executionRoleArn = var.iam_execution_role_arn
+    jobRoleArn = var.iam_job_role_arn
+    fargatePlatformConfiguration = {
+      platformVersion = "LATEST"
+    }
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group = aws_cloudwatch_log_group.cw_log_group.name
+      }
+    }
+    resourceRequirements = [{
+      type = "MEMORY"
+      value = "2048"
+    }, {
+      type = "VCPU",
+      value = "1"
+    }]
+    mountPoints = [{
+      sourceVolume = "input",
+      containerPath = "/data"
+      readOnly = false
+    }]
+    volumes = [{
+      name = "input"
+      efsVolumeConfiguration = {
+        fileSystemId = var.efs_file_system_ids["input"]
+        rootDirectory = "/"
+      }
+    }]
+  })
+}
+
+# Log group
+resource "aws_cloudwatch_log_group" "cw_log_group" {
+  name = "/aws/batch/job/${var.prefix}-setfinder/"
 }
